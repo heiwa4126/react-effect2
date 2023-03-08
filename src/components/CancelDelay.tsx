42f3be69c7032340b40fb0e0ad214a51daf1f20c
heiwa4126 <heiwa4126@gmail.com>
2023-03-08 15:20:40 +0900

    (snap)

--- src/components/CancelDelay.tsx
+++ src/components/CancelDelay.tsx
@@ -1,10 +1,35 @@
 import { useState } from "react";
 
-async function delay(mSec: number = 1000) {
+/**
+ * 指定時間後にpromiseをresolveする関数
+ * @param {number} [mSec=1000] 待ち時間
+ * @return {*}  {Promise<unknown>}
+ */
+async function delay(mSec: number = 1000): Promise<unknown> {
   return new Promise((resolve) => setTimeout(resolve, mSec));
 }
 
-export function CancelDelay() {
+/**
+ * delay()があんまりなんで、結果を返すようにしたもの
+ * @return {*}  {Promise<string>}
+ */
+async function d2(): Promise<string> {
+  return delay().then(() => {
+    return "done!";
+  });
+}
+
+export default function CancelDelay() {
+  return (
+    <>
+      <h1>CancelDelay</h1>
+      <CD2 />
+      <CD1 />
+    </>
+  );
+}
+
+function CD1() {
   const [msg, setMsg] = useState("...");
   const onClick = async () => {
     setMsg("waiting...");
@@ -14,7 +39,27 @@ export function CancelDelay() {
 
   return (
     <>
-      <h1>CancelDelay</h1>
+      <h2>CD1</h2>
+      <p>1秒後にpromiseを返す。途中で止められないし、ボタンの連打も防げない。</p>
+      <p>status: {msg}</p>
+      <div>
+        <button onClick={onClick}>start</button>
+      </div>
+    </>
+  );
+}
+
+function CD2() {
+  const [msg, setMsg] = useState("...");
+  const onClick = async () => {
+    setMsg("waiting...");
+    const s = await d2();
+    setMsg(s);
+  };
+
+  return (
+    <>
+      <h2>CD2</h2>
       <p>1秒後にpromiseを返す。途中で止められないし、ボタンの連打も防げない。</p>
       <p>status: {msg}</p>
       <div>
